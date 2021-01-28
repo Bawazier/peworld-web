b91d9e263afc7e5c90f695434c874d82294a8600
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-01-28 17:14:44 +0700

    creata home page

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,147 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
+import Link from "next/link";
+import {
+  FaCheckCircle,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+} from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className="text-7xl">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <Layout>
+      <section className="w-full max-h-screen grid grid-cols-2 gap-x-2 py-8 items-center justify-between">
+        <div className="flex flex-col space-y-4 w-auto">
+          <h1 className="font-sans font-semibold text-4xl">
+            Talenta terbaik negeri untuk perubahan revolusi 4.0
+          </h1>
+          <p className="font-sans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+            ipsum et dui rhoncus auctor.
+          </p>
+          <Link href="/">
+            <button className="font-sans w-2/5 bg-current-purple text-white rounded-md text-lg font-bold px-4 py-2">
+              Mulai Dari Sekarang
+            </button>
+          </Link>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <div className="">
+          <img src="/images/bgpromotion-a.png" className="w-full h-full" />
+        </div>
+      </section>
+      <section className="w-full h-screen grid grid-cols-2 gap-x-2 py-2 items-center justify-start">
+        <div className="">
+          <img src="/images/bgpromotion-b.png" className="w-full h-full" />
+        </div>
+        <div className="flex flex-col space-y-4 w-auto">
+          <h1 className="font-sans font-semibold text-3xl">
+            Kenapa harus mencari tallent di peworld
+          </h1>
+          <div className="flex space-x-4 items-center pt-8">
+            <FaCheckCircle className="text-current-purple" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="flex space-x-4 items-center">
+            <FaCheckCircle className="text-current-purple" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="flex space-x-4 items-center">
+            <FaCheckCircle className="text-current-purple" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="flex space-x-4 items-center">
+            <FaCheckCircle className="text-current-purple" />
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </section>
+      <section className="w-full h-screen flex space-x-2 items-center justify-end">
+        <div className="flex flex-col space-y-4 justify-end w-auto">
+          <h1 className="font-sans font-semibold text-3xl">Skill Tallent</h1>
+          <p className="font-sans max-w-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod
+            ipsum et dui rhoncus auctor.
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex space-x-4 items-center">
+              <FaCheckCircle className="text-yellow-500" />
+              <p>Java</p>
+            </div>
+            <div className="flex space-x-4 items-center">
+              <FaCheckCircle className="text-yellow-500" />
+              <p>Kotlin</p>
+            </div>
+            <div className="flex space-x-4 items-center">
+              <FaCheckCircle className="text-yellow-500" />
+              <p>PHP</p>
+            </div>
+            <div className="flex space-x-4 items-center">
+              <FaCheckCircle className="text-yellow-500" />
+              <p>10+ Bahasa lainnya</p>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <img src="/images/bgpromotion-c.png" className="w-full h-full" />
+        </div>
+      </section>
+      <section className="w-full h-screen flex flex-col space-y-10 py-2 items-center justify-start">
+        <h1 className="font-sans font-semibold text-3xl">
+          Their opinion about peworld
+        </h1>
+        <div className="flex items-center justify-center space-x-8">
+          <FaArrowAltCircleLeft className="text-current-purple text-4xl shadow-2xl" />
+          <div className="w-64 shadow-2xl flex flex-col items-center justify-center space-y-2 py-8 px-8">
+            <img
+              src="/images/person.png"
+              className="rounded-full h-24 w-24 flex items-center justify-center border-4 border-yellow-500"
+            />
+            <h1 className="font-sans font-semibold text-2xl">Harry Styles</h1>
+            <span className="text-gray-500">Web Developer</span>
+            <p className="text-gray-800 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              euismod ipsum et dui rhoncus auctor.
+            </p>
+          </div>
+          <div className="w-64 shadow-2xl flex flex-col items-center justify-center space-y-2 py-8 px-8">
+            <img
+              src="/images/person.png"
+              className="rounded-full h-24 w-24 flex items-center justify-center border-4 border-yellow-400"
+            />
+            <h1 className="font-sans font-semibold text-2xl">Harry Styles</h1>
+            <span className="text-gray-500">Web Developer</span>
+            <p className="text-gray-800 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              euismod ipsum et dui rhoncus auctor.
+            </p>
+          </div>
+          <div className="w-64 shadow-2xl flex flex-col items-center justify-center space-y-2 py-8 px-8">
+            <img
+              src="/images/person.png"
+              className="rounded-full h-24 w-24 flex items-center justify-center border-4 border-yellow-400"
+            />
+            <h1 className="font-sans font-semibold text-2xl">Harry Styles</h1>
+            <span className="text-gray-500">Web Developer</span>
+            <p className="text-gray-800 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              euismod ipsum et dui rhoncus auctor.
+            </p>
+          </div>
+          <FaArrowAltCircleRight className="text-current-purple text-4xl shadow-2xl" />
+        </div>
+      </section>
+      <section className="w-full h-screen flex items-center justify-center">
+        <div className="w-full flex justify-between items-center p-10 bg-current-purple rounded-tl-3xl rounded-br-3xl">
+          <h1 className="font-sans font-semibold text-3xl text-white max-w-sm">
+            Lorem ipsum dolor sit amet
+          </h1>
+          <Link href="/">
+            <button className="font-sans w-2/5 bg-white text-current-purple rounded-md text-lg font-bold px-4 py-2">
+              Mulai Dari Sekarang
+            </button>
+          </Link>
+        </div>
+      </section>
+    </Layout>
   );
 }
