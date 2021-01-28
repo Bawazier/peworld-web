c675ba1cfeae908e5accd3ed2ec004ab5b1e6840
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-01-28 17:13:21 +0700

    update package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+// tailwind.config.js
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "current-purple": "#5E50A1",
+      },
+    },
   },
   variants: {
     extend: {},
