4a47fbcad6e26085ad97b6a0a5ca179a055ee54b
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-01-31 10:05:52 +0700

    slicing header after login

--- components/layout/default-header.js
+++ components/layout/default-header.js
@@ -1,25 +1,44 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import Link from "next/link";
+import { FiMail, FiBell } from "react-icons/fi";
 
-function DefaultHeader() {
+function DefaultHeader({login}) {
   return (
     <header className="shadow-xl">
       <div className="flex flex-row mx-auto py-4 justify-between items-center w-4/5">
         <div>
           <img src="../icons/peworld-a.svg" className="w-36" />
         </div>
-        <div className="flex space-x-6">
-          <Link href="/">
-            <button className="font-sans border-2 border-current-purple text-current-purple rounded-md text-lg font-bold px-4 py-2">
-              Masuk
-            </button>
-          </Link>
-          <Link href="/">
-            <button className="font-sans bg-current-purple text-white rounded-md text-lg font-bold px-4 py-2">
-              Daftar
-            </button>
-          </Link>
-        </div>
+        {login ? (
+          <div className="flex items-center space-x-6">
+            <Link href="/">
+              <FiBell className="text-gray-500 text-2xl" />
+            </Link>
+            <Link href="/">
+              <FiMail className="text-gray-500 text-2xl" />
+            </Link>
+            <Link href="/">
+              <img
+                src="../images/person.png"
+                className="w-8 h-8 rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <div className="flex space-x-6">
+            <Link href="/">
+              <button className="font-sans border-2 border-current-purple text-current-purple rounded-md text-lg font-bold px-4 py-2">
+                Masuk
+              </button>
+            </Link>
+            <Link href="/">
+              <button className="font-sans bg-current-purple text-white rounded-md text-lg font-bold px-4 py-2">
+                Daftar
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   );
