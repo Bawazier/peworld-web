5035ee457d93922c8c9007c05430db5a93c2af6e
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-03-08 00:53:29 +0700

    update api

--- libs/api.js
+++ libs/api.js
@@ -5,19 +5,15 @@ import qs from "querystring";
 export const login = (data) => {
   return http().post("auth/login", qs.stringify(data));
 };
-
 export const registerWorker = (data) => {
   return http().post("auth/signup/2", qs.stringify(data));
 };
-
 export const registerRecruiter = (data) => {
   return http().post("auth/signup/3", qs.stringify(data));
 };
-
 export const forgotPassword = (data) => {
   return http().post("auth/reset/password", qs.stringify(data));
 };
-
 export const resetPassword = (id, data) => {
   return http().put(`auth/reset/password/${id}`, qs.stringify(data));
 };
@@ -36,29 +32,36 @@ export const getHome = async (
 
   return data;
 };
-
 export const getDetailsUser = async (token, id) => {
   const {data} = await http(token).get(`/home/${id}`);
   return data;
+};
+
+// * PROFILE WORKER API
+export const getProfile = async (token) => {
+  const { data } = await http(token).get("worker/account");
+  return data;
+};
+export const updateProfile = (token, data) => {
+  return http(token).patch("worker/account", qs.stringify(data));
+};
+export const updateImageProfile = async (token, data) => {
+  return http(token).patch("worker/account", data);
 };
 
 // * MESSAGE API
 export const getRecipientProfile = (token, id) => {
   return http(token).get("home/" + id);
 };
-
 export const getAllList = (token, page) => {
   return http(token).get("message/list/person?" + qs.stringify({page}));
 };
-
 export const getPrivateMessage = (token, id, page) => {
   return http(token).get("message/list/chat/" + id + "?" + qs.stringify({page}));
 };
-
 export const sendChat = (token, id, message) => {
   return http(token).post("message/chat/" + id, qs.stringify({ message }));
 };
-
 export const readChat = (token, id) => {
   return http(token).patch("message/read/" + id);
 };
