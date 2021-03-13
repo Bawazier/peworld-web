c48c35a5e558e536835aa4f50db5e7b0535df3f5
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-03-13 16:44:52 +0700

    create loading component

--- components/handle/mutateLoading.js
+++ components/handle/mutateLoading.js
@@ -1,8 +1,29 @@
+/* eslint-disable react/prop-types */
 import React from "react";
-import {FaReact} from "react-icons/fa";
+import { VscLoading } from "react-icons/vsc";
 
-function loading() {
-  return <FaReact className="animate-spin text-center text-2xl w-full" />;
+function MutateLoading({containerWidth, containerHeight, direction, iconSize, title, titleSize}) {
+  return (
+    <div
+      className={`w-${containerWidth} h-${containerHeight} flex flex-${direction} items-center justify-center space-y-4 space-x-4`}
+    >
+      <VscLoading className={`animate-spin text-black text-${iconSize}`} />
+      <h1
+        className={`font-sans text-black text-${titleSize} font-bold animate-bounce`}
+      >
+        {title}
+      </h1>
+    </div>
+  );
 }
 
-export default loading;
+MutateLoading.defaultProps = {
+  containerWidth: "full",
+  containerHeight: "full",
+  direction: "col",
+  iconSize: "9xl",
+  title: "Mohon Tunggu Sebentar",
+  titleSize: "2xl",
+};
+
+export default MutateLoading;
